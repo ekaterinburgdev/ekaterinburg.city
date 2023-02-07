337b8e44f05cf8dadb7accac3c45caaf8d80661d
Anton Akhatov <anton@gcor.ru>
2023-02-07 21:45:08 +0000

    feature: animated logo

---
+++ src/projectspanel.tsx
@@ -0,0 +1,15 @@
+import { AnimatedLogo } from "ekb";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "ekb/style.css";
+
+const rootNode = document.getElementById("logo");
+if (rootNode) {
+  const root = ReactDOM.createRoot(rootNode);
+  root.render(
+    React.createElement(AnimatedLogo, {
+      radius: "clamp(80px, 30vw, 256px)",
+      withLogoHover: false,
+    })
+  );
+}
